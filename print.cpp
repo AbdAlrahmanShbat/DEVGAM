7c57b0c50d277447e8eeaa88dae36e255294ecf7
abdalrahman shbat <abdalrahman.shbat952@gmail.com>
2024-09-21 01:45:01 +0300

    fainal

--- print.cpp
+++ print.cpp
@@ -19,23 +19,17 @@ time(&tt);
 
 ti = localtime(&tt);
 
-string formattedTime = asctime(ti);
+
+    string formattedTime = asctime(ti);
 
 formattedTime.pop_back();
 
 cout << "[" << formattedTime << "]" << customMessage << endl;
-
-} 
-/* ???? */
+}
 int main()
 {
-    /* when u use function u just call the name and the parameters ?>?????? */
-    //DONE? run the app ??
-    // I DID can u see the termina  run again ?????
-// i did
-    printTimeWithMessage/* <- this is the name and this is the parameters -> */   ("your custom message goes here.");
-    // try and run the code there is error , you check errors like this, from the terminal bellow, good// 
 
-     // where to write my msg ? in the display termenal after i run the code? u will see the msg bellow, but seems u have errors 
+    printTimeWithMessage("your custom message goes here.");
+
     return 0;
 }
